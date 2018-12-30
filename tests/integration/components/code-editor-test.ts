6437b2e9a15f7741552ebae5b3545d8d87ecec2e
Mike North <michael.l.north@gmail.com>
2018-12-29 19:00:28 -0800

    chore: tests

--- tests/integration/components/code-editor-test.ts
+++ tests/integration/components/code-editor-test.ts
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, waitUntil, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | code-editor', function(hooks) {
@@ -10,17 +10,46 @@ module('Integration | Component | code-editor', function(hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{code-editor}}`);
+    await render(hbs`{{code-editor
+  code="let x: string = 'foo';"
+  language="typescript"
+}}`);
+    await settled();
+    await waitUntil(() => {
+      const frame = document.querySelector('iframe');
+      if (!frame) return false;
+      const frameWin = frame.contentWindow;
+      if (!frameWin) return false;
+      return frameWin.document;
+    });
 
-    assert.equal(('' + this.element.textContent).trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#code-editor}}
-        template block text
-      {{/code-editor}}
-    `);
-
-    assert.equal(('' + this.element.textContent).trim(), 'template block text');
+    await waitUntil(
+      () => {
+        const frame = document.querySelector('iframe');
+        if (!frame) throw new Error('No frame');
+        const frameWin = frame.contentWindow;
+        if (!frameWin) throw new Error('No frame window');
+        const frameDoc = frameWin.document;
+        const x = frameDoc.querySelector('.monaco-editor');
+        return !!x;
+      },
+      {
+        timeout: 60000,
+        timeoutMessage: 'Waiting for line content'
+      }
+    );
+    const frame = document.querySelector('iframe');
+    if (!frame) throw new Error('No frame');
+    const frameWin = frame.contentWindow;
+    if (!frameWin) throw new Error('No frame window');
+    const frameDoc = frameWin.document;
+    const linesContent = frameDoc.querySelector<HTMLDivElement>(
+      '.lines-content'
+    );
+    if (!linesContent) throw new Error('No frame content');
+    assert.equal(
+      linesContent.innerText.replace(/\s/g, ' '),
+      "let x: string = 'foo';"
+    );
   });
 });
